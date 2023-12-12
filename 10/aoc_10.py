af3c01fd797e97c8bf8b6ea0e2c717de644cbaaa
Jan Kleisl <xkleis00@gmail.com>
2023-12-12 20:36:15 +0100

    part 1 done

--- 10/aoc_10.py
+++ 10/aoc_10.py
@@ -1,5 +1,3 @@
-#potreba odtrackovat output kde nastane chyba 
-
 import numpy as np
 
 go_right = ('-', 'J', '7')
@@ -7,9 +5,11 @@ go_left = ('-', 'L', 'F')
 go_top = ('|', 'F', '7')
 go_bot = ('|', 'L', 'J')
 
-r_index, c_index, cur_pipe, pipe_cnt = -1, -1, 's', 0
+r_index, c_index, cur_pipe, pipe_cnt, tiles_in_loop = -1, -1, 's', 0, 0
+pipe_border = []
+is_in = False 
 
-with open('input_2.txt') as f:
+with open('input_0.txt') as f:
     lines = f.readlines()
     pipes_map = np.zeros((len(lines), len(lines[0]) - 1), dtype='object')
     for line in lines:
@@ -141,19 +141,44 @@ with open('input_2.txt') as f:
         
         elif cur_pipe == 'F':
             if ex_pipe in go_bot:
-                ex_pipe = cur_pipe
-                cur_pipe = pipes_map[starting_point[0]][starting_point[1] + 1]
-                starting_point = (starting_point[0], starting_point[1] + 1)
-                from_right = False
+                if ex_pipe == 'J' and from_right:
+                    ex_pipe = cur_pipe
+                    cur_pipe = pipes_map[starting_point[0] + 1][starting_point[1]]
+                    starting_point = (starting_point[0] + 1, starting_point[1])
+                    from_bot = False
+                else:
+                    ex_pipe = cur_pipe
+                    cur_pipe = pipes_map[starting_point[0]][starting_point[1] + 1]
+                    starting_point = (starting_point[0], starting_point[1] + 1)
+                    from_right = False
             else:
                 ex_pipe = cur_pipe
                 cur_pipe = pipes_map[starting_point[0] + 1][starting_point[1]]
                 starting_point = (starting_point[0] + 1, starting_point[1])
                 from_bot = False
             pipe_cnt += 1
+        
+        pipe_border.append((starting_point[0], starting_point[1]))
+        print(cur_pipe, starting_point[0], starting_point[1])
+
+    for row in range(len(lines)):
+        for col in range(len(lines[0]) - 1):
+            print(row, col)
+            if (row, col) in pipe_border:
+                print("xd")
+            if (row, col - 1) in pipe_border:
+                is_in = not is_in
+                print("toggled in", is_in, row, col)
+            elif (row, col - 1) not in pipe_border:
+                is_in = not is_in
+                print("toggled in", is_in, row, col)
+        
+            if is_in:
+                tiles_in_loop += 1
+                print(is_in, (row, col), tiles_in_loop)
+        is_in = False
             
-        print(cur_pipe, ex_pipe)
-    
-    print(pipes_map)
-    print(starting_point)
-    print(pipe_cnt/2)
+    print(pipe_border, len(pipe_border))    
+    print("PART 1:", pipe_cnt/2)
+    print("PART 2:", tiles_in_loop)
+    print((len(lines), len(lines[0]) - 1))
